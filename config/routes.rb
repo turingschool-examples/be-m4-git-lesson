1bc59688597c93e57e1d6e601b00fe1585893cc7
Dione Wilson <dionew1@gmail.com>
2017-11-01 17:34:14 -0600

    Create landing page and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root to:  'landing#index'
   resources :items, only: [:index, :show]
   resource  :cart
-  # resources :categories, only: :show
 
   get '/:category', to: 'categories#show', param: :slug, as: "category"
 end
