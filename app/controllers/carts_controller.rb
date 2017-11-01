076b93afef067b5f9db88aad05695460bce76853
Hugh Morrison <hmorri32@gmail.com>
2017-11-01 15:30:50 -0600

    closes #29, flash message for successful remove from cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,11 +13,7 @@ class CartsController < ApplicationController
   def destroy
     @item = Item.find(params[:item_id])
     @cart.remove_item(params[:item_id])
-    # flash[:success] = "Successfully removed #{view_context.link_to("#{@item.title}", item_path(@item))}from your cart.".html_safe
-    flash[:success] = "Successfully removed <a href='#{item_path(@item)}'> #{@item.title}</a> ".html_safe
+    flash[:success] = "Successfully removed <a href=#{item_path(@item)}>#{@item.title}</a> from your cart."
     redirect_to cart_path
   end
 end
-
-
-"#{link_to @item.title, :controller => 'items', :action => 'show', :item => update.id}."
