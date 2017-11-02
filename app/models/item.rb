bd56e3435e9b1c0ab9e7bf075dc7a1b29c159d32
Hugh Morrison <hmorri32@gmail.com>
2017-11-02 13:24:04 -0600

    num to currency on all item.price instances

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
 
   validates_uniqueness_of :title
 
-  # validates :image, attachment_presence: true
+  validates :image, attachment_presence: true
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create(title: 'Little Cacti')
+category = Category.all.first
+category.items.create(title: 'Pink Cactus', description: "It has a little flower on top", price: 10.95, image: File.open('app/assets/images/2.jpg', 'r'))
+category.items.create(title: 'Pretty Cactus', description: 'It comes in a geometric glass pot.', price: 20.95, image: File.open('app/assets/images/1.jpg', 'r'))
+category.items.create(title: 'Basic Cactus', description: 'It comes in a ceramic pot.', price: 6.95, image: File.open('app/assets/images/3.jpg', 'r'))
