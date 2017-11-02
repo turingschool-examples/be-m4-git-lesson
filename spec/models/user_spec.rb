90740b66660258faecf76b068dd85c355e446429
Dione Wilson <dionew1@gmail.com>
2017-11-02 11:32:19 -0600

    Complte User Spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  describe "Validations" do
-    context "valid attributes" do
-      it "is valid with email, first_name, last_name, address, role, password" do
-        user = User.new(email: "name@me.com",
-                    first_name: "John",
-                    last_name: "Doe",
-                    address: "123 House Lane Denver, CO",
-                    role: 0,
-                    password: "test")
-        expect(user).to be_valid
-      end
-    end
+  describe "validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:role) }
+    it { should validate_presence_of(:password) }
+    it { should have_secure_password }
+    it { should define_enum_for(:role) }
   end
 end
