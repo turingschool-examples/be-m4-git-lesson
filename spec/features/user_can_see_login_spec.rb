2842d0dc28a5facc8e383260f692cc42d9129e9f
Dione Wilson <dionew1@gmail.com>
2017-11-02 14:21:13 -0600

    Create Login, User controller, User show page, User route for show

--- spec/features/user_can_see_login_spec.rb
+++ spec/features/user_can_see_login_spec.rb
@@ -6,4 +6,31 @@ RSpec.feature "User can see a login" do
 
     expect(page).to have_link("Log In")
   end
+
+  scenario "when I click login I should be on login page" do
+    visit root_path
+
+    click_on "Log In"
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content("Email")
+    expect(page).to have_content("Password")
+  end
+
+  scenario "registered user can log in" do
+    user = User.create(first_name: "John",
+                       last_name: "Doe",
+                       email: "John@Doe.com",
+                       password: "test",
+                       address: "123 House Lane Denver, CO 80231")
+    visit login_path
+    
+    fill_in "session[email]", with: user.email
+    fill_in "session[password]", with: user.password
+
+    click_on  "Submit"
+
+    expect(current_path).to eq user_path(user)
+    expect(page).to have_content("Logged in as #{user.first_name}")
+  end
 end
