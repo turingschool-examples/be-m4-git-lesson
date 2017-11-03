61aa753380e68614ee8b47fec514745a707410a8
Hugh Morrison <hmorri32@gmail.com>
2017-11-02 18:39:29 -0600

    fixes user show page, now uses current_user as opposed to User.find

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to:  'landing#index'
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get '/dashboard', to: 'users#show'
+  get    '/login',     to: 'sessions#new'
+  post   '/login',     to: 'sessions#create'
+  delete '/logout',    to: 'sessions#destroy'
+  get    '/dashboard', to: 'users#show'
+
   resources :items, only: [:index, :show]
   resource  :cart
   resources :users, only: [:new, :create]
