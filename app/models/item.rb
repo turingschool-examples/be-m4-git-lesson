6f607e327c79ad188287d12f98ac290841fa7c58
Hugh Morrison <hmorri32@gmail.com>
2017-11-04 14:09:38 -0600

    styles item show page

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
 
   validates :image, attachment_presence: true
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100#" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   enum status: [ "active", "retired" ]
