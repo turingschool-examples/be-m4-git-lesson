c0a26fec5f79761bb90d4ce25626b6fa0bdc0a2d
Dione Wilson <dionew1@gmail.com>
2017-11-04 16:30:25 -0600

    Index Orders Complete

--- spec/features/user_sees_checkout_spec.rb
+++ spec/features/user_sees_checkout_spec.rb
@@ -33,17 +33,9 @@ RSpec.feature "Visitor visits '/cart'" do
     click_on "Add to Cart"
     click_link "Cart"
     click_on "Checkout"
-    save_and_open_page
 
     expect(current_path).to eq orders_path
     expect(page).to have_content("Order was successfully placed")
     expect(page).to have_content(Order.last.id)
   end
 end
-
-# When I am logged in And I visit my cart
-# And when I click “Checkout”
-# Then the order should be placed
-# And my current page should be “/orders”
-# And I should see a message “Order was successfully placed”
-# And I should see the order I just placed in a table
