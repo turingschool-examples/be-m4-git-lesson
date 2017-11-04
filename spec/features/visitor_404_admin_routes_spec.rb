47ecf3c9dadf276c92bdcd0025d82bf7b7f1821f
Hugh Morrison <hmorri32@gmail.com>
2017-11-03 19:29:09 -0600

    extrat non admin tests to own feature

---
+++ spec/features/visitor_404_admin_routes_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "As a non admin" do
+  context "as a visitor" do
+    it 'I visit admin/dashboard and receive status 404' do
+      visit admin_dashboard_path
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
+
+  context "as an authenticated user" do
+    it 'I visit admin/dashboard and receive status 404' do
+    user = User.create(first_name: 'Django',
+                        last_name: 'Unchained',
+                        email: 'cool',
+                        address: 'wherever',
+                        password: 'django',
+                        role: 0)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit admin_dashboard_path
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
+end
