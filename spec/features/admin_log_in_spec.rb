823c8f4e21c119a8cd10a9f1660508ba43afc525
Hugh Morrison <hmorri32@gmail.com>
2017-11-03 19:12:48 -0600

    admin logs in and is redirected to admin dashboard spec

---
+++ spec/features/admin_log_in_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "An admin logs in" do
+  scenario "admin redirected to admin dashboard page when logged in" do
+    admin = User.create(first_name: 'Django',
+                        last_name: 'Unchained',
+                        email: 'cool',
+                        address: 'wherever',
+                        password: 'django',
+                        role: 1)
+
+    visit login_path
+
+    fill_in "session[email]",    with: admin.email
+    fill_in "session[password]", with: admin.password
+    click_on "Submit"
+
+    # expect(current_path).to eq(admin_dashboard_path)
+  end
+end
+
+# def login_as(user)
+#   visit login_path
+#   fill_in "Username", with: user.username
+#   fill_in "Password", with: user.password
+#   click_button "Login"
+# end
