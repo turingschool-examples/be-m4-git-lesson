bcafd3eee0e309048459582bc496afb6269bb9fe
Hugh Morrison <hmorri32@gmail.com>
2017-11-05 15:32:48 -0700

    conditionally render order by status in admin::orders index

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,33 +1,12 @@
 class Admin::OrdersController < Admin::BaseController
-  before_action :require_admin
-
   def index
     @orders = delegate_orders
-    p params
   end
 
   def delegate_orders
-    return Order.ordered   if params["status"] == "0"
-    return Order.paid      if params["status"] == "1"
-    return Order.cancelled if params["status"] == "2"
-    return Order.completed if params["status"] == "3"
+    return Order.ordered   if params[:status] == "0"
+    return Order.paid      if params[:status] == "1"
+    return Order.cancelled if params[:status] == "2"
+    return Order.completed if params[:status] == "3"
   end
-
-  def cancelled_orders
-
-  end
-
-  def paid_orders
-
-  end
-
-  def completed_orders
-
-  end
-
-  def cancelled_orders
-
-  end
-
-
 end
