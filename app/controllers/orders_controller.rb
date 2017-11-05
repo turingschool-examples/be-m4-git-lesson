ee5b67c18f3d13da22a4af5edef761ac64127eb6
Dione Wilson <dionew1@gmail.com>
2017-11-05 10:06:03 -0700

    Fixed failing test by adding to orders path in the cart show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,23 @@
 class OrdersController < ApplicationController
-  before_action :check_user
 
   def index
-    @orders = current_user.orders.reverse_order
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
   end
 
   def create
-    order = Order.new(user: current_user)
+  order = Order.new(user: current_user)
     if order.save
-      items = Item.find(session[:cart].keys.map { |key| key.to_i })
+      items = Item.find(params[:cart].keys.map { |key| key.to_i })
       items.each { |item| order.items << item }
       flash[:success] = "Order was successfully placed"
       redirect_to orders_path
     else
-      flash[:error] = "An error occured please replace your order."
+      flash[:error] = "An error occured please re-place your order."
       redirect_to cart_path
     end
-  end
-
-  private
-  def check_user
-    redirect_to login_path if !current_user
   end
 end
