2f21df957271da62ba2df4a70ce17cf46725567e
Dione Wilson <dionew1@gmail.com>
2017-11-05 19:39:04 -0700

    Update Unit tests for Order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -12,4 +12,51 @@ RSpec.describe Order do
     it { should have_many(:items) }
     it { should respond_to(:items) }
   end
+
+  describe "class methods" do
+    it ".total_ordered" do
+      user = create(:user)
+      create(:order, status:0, user: user)
+      create(:order, status:0, user: user)
+
+      expect(Order.total_ordered).to eq(2)
+    end
+
+    it ".total_paid" do
+      user = create(:user)
+      create(:order, status:1, user: user)
+      create(:order, status:1, user: user)
+
+      expect(Order.total_paid).to eq(2)
+    end
+
+    it ".total_cancelled" do
+      user = create(:user)
+      create(:order, status:2, user: user)
+      create(:order, status:2, user: user)
+
+      expect(Order.total_cancelled).to eq(2)
+    end
+
+    it ".total_completed" do
+      user = create(:user)
+      create(:order, status:3, user: user)
+      create(:order, status:3, user: user)
+
+      expect(Order.total_completed).to eq(2)
+    end
+  end
+
+  describe "instance methods" do
+    it "#formate_date" do
+      user  = create(:user)
+      order = create(:order, user: user)
+      comparison = order.format_date(order.created_at).split("/")
+
+      expect(comparison[0].to_i).to be_between(0, 13).exclusive
+      expect(comparison[1].to_i).to be_between(0, 32).exclusive
+      expect(comparison[2].length).to eq(4)
+      expect(comparison[2].to_i).to be_kind_of(Integer)
+    end
+  end
 end
