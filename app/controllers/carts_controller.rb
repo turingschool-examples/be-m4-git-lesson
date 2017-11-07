061f748e63855cf1119ffeb3bb3180a2943864cf
Iza Jimenez <izavetha@gmail.com>
2017-11-06 17:18:38 -0700

    Modified flash messages. Red for remove, green for add

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,7 +3,7 @@ class CartsController < ApplicationController
     item = Item.find(params[:item_id])
     @cart.add_item(item.id)
     session[:cart] = @cart.contents
-    flash[:notice] = "#{item.title} has been added to your cart!"
+    flash[:success] = "<a href=#{item_path(item)}>#{item.title}</a>  has been added to your cart!"
     redirect_to items_path
   end
 
@@ -14,7 +14,7 @@ class CartsController < ApplicationController
   def destroy
     @item = Item.find(params[:item_id])
     @cart.remove_item(params[:item_id])
-    flash[:success] = "Successfully removed <a href=#{item_path(@item)}>#{@item.title}</a> from your cart."
+    flash[:notice] = "Successfully removed <a href=#{item_path(@item)}>#{@item.title}</a> from your cart."
     redirect_to cart_path
   end
 
@@ -23,11 +23,11 @@ class CartsController < ApplicationController
     when params[:increment_me]
       @item = Item.find(params[:increment_me])
       @cart.add_item(@item.id)
-      flash[:notice] = "#{@item.title} has been added to your cart!"
+      flash[:success] = "<a href=#{item_path(@item)}>#{@item.title}</a> has been added to your cart!"
     when params[:decrement_me]
       @item = Item.find(params[:decrement_me])
       @cart.decrease_quantity(@item.id)
-      flash[:notice] = "You removed #{@item.title} from your cart!"
+      flash[:notice] = "<a href=#{item_path(@item)}>#{@item.title}</a> has been removed from your cart!"
     end
     redirect_to cart_path
   end
