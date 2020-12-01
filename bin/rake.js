447a6d985c3487786a69a4cdb415b8d46fce21fb
Taylor Johnson <taylorjohnson141@gmail.com>
2020-12-01 10:36:28 -0700

    remove-rake-method,change rake to js, add js funtunality to rake

---
+++ bin/rake.js
@@ -0,0 +1,9 @@
+function sayHi (user,greeting){
+console.log(greeting)
+return `${user} has been greeted`
+}
+
+function sayBye (user,adios){
+  console.log(adios)
+  return `${user} has been bied`
+}
