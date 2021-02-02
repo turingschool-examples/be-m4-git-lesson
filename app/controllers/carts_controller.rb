1b7782a67d2915e2bb15a0263c932a5a701527ff
Cooper Terrones <cooper.terrones@outlook.com>
2021-02-02 11:26:48 -0700

    remove method 3

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,4 @@
 class CartsController < ApplicationController
-  def create
-    item = Item.find(params[:item_id])
-    @cart.add_item(item.id)
-    session[:cart] = @cart.contents
-    flash[:success] = "<a href=#{item_path(item)}>#{item.title}</a> <span>has been added to your cart!</span>"
-    redirect_to items_path
-  end
 
   def show
     @items = @cart.retrieve_items
