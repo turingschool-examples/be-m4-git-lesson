7600cfa02d1845a408b7818d77dfb97fe55723ea
Elizabeth Hahn <elizhahn87@gmail.com>
2021-05-10 20:14:39 -0600

    Delete code from schema file

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,6 @@
 
 
-ActiveRecord::Schema.define(version: 20171108000258) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "categories", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug"
-  end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
